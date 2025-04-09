0a18947c937d1a629d7171f59988e0b50b2caa2a
Andri Soenoyo <49144391+AndriSoenoyo@users.noreply.github.com>
2025-04-09 06:10:56 +0000

    update gold tier threshold

--- models/silver/customer_tiers.sql
+++ models/silver/customer_tiers.sql
@@ -6,7 +6,7 @@ AS
 SELECT
 	TRANSFORM.Customer_ID, TRANSFORM.Customer_Name, TRANSFORM.TotalSalesPerCustomer, CASE
 			WHEN TotalSalesPerCustomer >= 15000 THEN 'Platinum'
-			WHEN TotalSalesPerCustomer >= 10000 THEN 'Gold'
+			WHEN TotalSalesPerCustomer >= 11000 THEN 'Gold'
 			WHEN TotalSalesPerCustomer >= 4000 THEN 'Silver'
 			ELSE 'Bronze'
 	END AS CustomerTier 
